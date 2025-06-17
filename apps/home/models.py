8a135f510e7af4d83a077bf5b72c4c783a02e647
DamarisEspinosaCastro <damarisespinosa0406@gmail.com>
2025-06-17 16:58:09 -0600

    modelo de factura corregido

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,39 +1,40 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
 from django.contrib.auth.models import User
+import os
 
-# Create your models here.
 class Factura(models.Model):
     ESTATUS_CHOICES = [
-        ('subida', 'Subida'), 
-        ('valida', 'Validada'),
+        ('subida', 'Subida'),
         ('aprueba', 'Aprobada'),
+        ('deniega','Denegada')
     ]
-
+    id = models.BigAutoField(primary_key=True)
     estatus = models.CharField(
         max_length=10, 
-        choices=ESTATUS_CHOICES,
-        default='subida',
+        choices=ESTATUS_CHOICES, 
+        default='subida'
     )
     fecha = models.DateTimeField(auto_now_add=True)
     cliente = models.CharField(max_length=100)
     usuario = models.ForeignKey(
         User, 
         on_delete=models.CASCADE,
-        related_name='facturas',
+        related_name='facturas'
     )
-    nombreArchivo = models.CharField(max_length=75)
-    extension = models.FileField(upload_to='facturas/')
+    nombre_archivo = models.CharField(max_length=255, editable=False) 
+    archivo = models.FileField(upload_to='facturas/')
     total = models.DecimalField(max_digits=10, decimal_places=2)
     direccion = models.TextField()
     concepto = models.TextField()
 
-    def __str__(self):
-        return f"{self.nombreArchivo}.{self.extension} - {self.cliente}"
-    
+    def save(self, *args, **kwargs):
+        if self.archivo and not self.nombre_archivo:
+            nombre_completo = self.archivo.name
+            nombre_base, extension = os.path.splitext(nombre_completo)
+            self.nombre_archivo = nombre_base
+            self.extension = extension[1:] 
+        super().save(*args, **kwargs)
 
-    
+    def __str__(self):
+        return f"{self.nombre_archivo}.{self.extension} - {self.cliente}"
+
